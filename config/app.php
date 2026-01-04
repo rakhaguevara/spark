99cb6fba2150607b72c32288b914a8a953f5f190
rakhaPC <rakha.guevara2505@gmail.com>
2026-01-04 20:30:17 +0700

    initial docker to my project

--- config/app.php
+++ config/app.php
@@ -1,6 +1,7 @@
 <?php
 // Sesuaikan dengan lokasi folder Anda
-define('BASEURL', 'http://localhost/spark');
+// Sesuaikan dengan lokasi folder Anda, atau gunakan env var untuk Docker
+define('BASEURL', getenv('BASEURL') ?: 'http://localhost/spark');
 
 // Atau jika pakai virtual host: 
 // define('BASEURL', 'http://spark.test');
